551de6398a65624452003dc29fd333f7a7e1583c
Brayan <brayanleal21@hotmail.com>
2020-01-15 10:45:22 -0600

    No era core

--- Lab0/Lab0/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Lab0.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Lab0/Lab0/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab0.WebApiApplication" Language="C#" %>
