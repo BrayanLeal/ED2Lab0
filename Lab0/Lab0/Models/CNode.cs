97cb9a742a3da0529667fa3e36e6027b1a8be110
Brayan <brayanleal21@hotmail.com>
2020-01-17 09:42:46 -0600

    Node

---
+++ Lab0/Lab0/Models/CNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab0.Models
+{
+    public class CNode<T>
+    {
+        //aqui lo de movie
+        public CNode<T> next;
+        public Movie Data;
+       
+    }
+}
