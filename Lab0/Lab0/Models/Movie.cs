3de50dc7677ab312dd269ed11e511a5de0b17e94
Brayan <brayanleal21@hotmail.com>
2020-01-17 08:22:13 -0600

    1st try

---
+++ Lab0/Lab0/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab0.Models
+{
+    public class Movie
+    {
+        public int Year { get; set; }
+        public string Title { get; set; }
+        public string Dir { get; set; }
+    }
+}
